adcd34eec43d331666cf151219410383517e4b16
MTVA <marcostadeu33@gmail.com>
2019-08-05 16:46:23 -0300

    Init Project

---
+++ src/main/java/com/marcosaragao/springgoodpraticescourse/SpringgoodpraticescourseApplication.java
@@ -0,0 +1,13 @@
+package com.marcosaragao.springgoodpraticescourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgoodpraticescourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgoodpraticescourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcosaragao/springgoodpraticescourse/SpringgoodpraticescourseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marcosaragao.springgoodpraticescourse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringgoodpraticescourseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
