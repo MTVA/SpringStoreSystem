4c19108ca77f68da542199a819b0f295754ea929
MTVA <marcostadeu33@gmail.com>
2019-08-06 14:12:33 -0300

    Create instance operation

--- src/main/java/com/marcosaragao/springgoodpraticescourse/SpringgoodpraticescourseApplication.java
+++ src/main/java/com/marcosaragao/springgoodpraticescourse/SpringgoodpraticescourseApplication.java
@@ -1,13 +1,31 @@
 package com.marcosaragao.springgoodpraticescourse;
 
+import com.marcosaragao.springgoodpraticescourse.domain.Categoria;
+import com.marcosaragao.springgoodpraticescourse.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringgoodpraticescourseApplication {
+public class SpringgoodpraticescourseApplication implements CommandLineRunner {
+
+    @Autowired
+    CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringgoodpraticescourseApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Games");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+    }
 }
